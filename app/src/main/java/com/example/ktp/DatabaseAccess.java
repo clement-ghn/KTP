63f84994287b16a46a49eb132f956f463465f007
clement_ghn <ent.clementgehin@gmail.com>
2022-02-08 17:18:11 -0500

    #test

---
+++ app/src/main/java/com/example/ktp/DatabaseAccess.java
@@ -0,0 +1,49 @@
+package com.example.ktp;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+
+public class DatabaseAccess {
+
+    private DatabaseOpenHelper openHelper;//Declaration de DatabaseOpenHelper
+    private SQLiteDatabase db;//Objt SQLite db
+    private static DatabaseAccess instance;
+    Cursor c = null; //contient resultat de requête sur une bdd
+
+    private DatabaseAccess(Context context){//Création d'une BDD
+        this.openHelper = new DatabaseOpenHelper(context);
+    }
+
+    public static DatabaseAccess getInstance(Context context){//Singleton retournant l'instance de la classe
+        if(instance==null){
+            instance = new DatabaseAccess(context);
+        }
+        return  instance;
+    }
+
+    public void open(){//Ouverture de la BDD
+        this.db=openHelper.getWritableDatabase();
+    }
+
+    public void close(){//Fermeture de la BDD
+        if(db!=null){
+            this.db.close();
+        }
+    }
+
+
+    //Requête vers la BDD
+
+    public String getpartie(String partie){
+        c=db.rawQuery("select id from exercices where partie = ?",new String[]{partie});
+
+        c.moveToFirst();
+        return  c.getString(0);
+    }
+
+
+
+
+
+}
